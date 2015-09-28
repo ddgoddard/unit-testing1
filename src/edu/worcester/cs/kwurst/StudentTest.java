b9fa4d02b7132016c1969577cabd3d30a32369a5
ddgoddard <dgoddard@worcester.edu>
2015-09-28 15:28:29 -0400

    Added edited StudentTest.java

--- src/edu/worcester/cs/kwurst/StudentTest.java
+++ src/edu/worcester/cs/kwurst/StudentTest.java
@@ -26,24 +26,58 @@ public class StudentTest {
 	}
 
 	@Test
+	 public void testGetStudentCount(){
+	         int current = Student.getStudentCount();
+	          Student student3 = new Student ("Bill", "Nye");
+	           assertEquals(current +1, Student.getStudentCount());
+	 }
+	
+	@Test
 	public void testGetCurrentEarnedCr() {
 		int credits = 120; 
 		student1.setCurrentEarnedCr(credits);
 		 assertEquals(120, student1.getCurrentEarnedCr());
 		}
+	
+	@Test
+	public void testCurrentEarnedCredits(){
+		student1.setCurrentEarnedCr(-5);
+		assertTrue(student1.getCurrentEarnedCr() >= 0);
+	}//Prevents faulty error on admins account
+	
 	@Test
 	public void testGetCurrentRemainingCr() {
-		int credits = 20;
-		student1.setAnticipatedAdditionalCr(credits);
-		 assertTrue(student1.getAnticipatedRemainingCr() == 0);
+		student1.setCurrentEarnedCr(20);
+		 assertTrue(student1.getCurrentRemainingCr() == 100);
 		}
 	
 	@Test
-	public void testAnticipatedAdditionalCr() {
-		int credits = 90;
-		student2.setAnticipatedAdditionalCr(credits);
-		assertEquals(90, student2.getAnticipatedAdditionalCr());
+	public void testGetCurrentRemainingCrOver() {
+		student1.setCurrentEarnedCr(125);
+		 assertTrue(student1.getCurrentRemainingCr() == 0);
+		}//If a Student earns more than 120 credits, they did not graduate according to code.
+	
+	@Test
+	public void testGetCurrentRemainingCrNone() {
+		student1.setCurrentEarnedCr(0);
+		 assertTrue(student1.getCurrentRemainingCr() == 120);
+		}
+	
+	@Test
+	public void testAnticipatedRemainingCrLow() {
+		student2.setCurrentEarnedCr(0);
+		student2.setAnticipatedAdditionalCr(0);
+		assertEquals(120, student2.getAnticipatedRemainingCr());
 	}
+	
+	@Test
+	public void testAnticipatedRemainingCrHigh() {
+		student2.setCurrentEarnedCr(100);
+		student2.setAnticipatedAdditionalCr(20);
+		assertEquals(0, student2.getAnticipatedRemainingCr());
+	}
+	
+	
 	
 	@Test
 	public void testReadyToGraduate(){
@@ -56,5 +90,11 @@ public class StudentTest {
 		assertFalse(student1.getCurrentEarnedCr() >= 120 || student1.getGpa() >= 2.0);
 		assertFalse(student1.getLascComplete() || student1.getMajorComplete());
 	}
+	
+	@Test
+	public void testGpaValid(){
+		student1.setGpa(4.3);
+		assertTrue(student1.getGpa() >= 0 && student1.getGpa() <= 4.0);
+	}
+	
 }
-
